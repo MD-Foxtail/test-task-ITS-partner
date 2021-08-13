edbf79d85b44c4ebad1fe6f9e47ad7ccc56cdb6a
MD-foxtail <md.foxtail@yandex.ru>
2021-08-13 08:27:41 +0300

    http request added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <curl/curl.h>
 int main()
 {
-  std::cout << "hello world\n";
+  CURL* curl;
+  CURLcode responce;
+  curl = curl_easy_init();
+  curl_easy_setopt(curl, CURLOPT_HEADER, 1);
+  curl_easy_setopt(curl, CURLOPT_URL, "https://www.google.ru/");
+  responce = curl_easy_perform(curl);
+  curl_easy_cleanup(curl);
 }
