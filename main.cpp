67167fc30e8223425d19d2587b844d05982a7936
MD-foxtail <md.foxtail@yandex.ru>
2021-08-14 04:49:55 +0300

    0.4.0 This is an intermediate stage -Added version numbers (for fun) -Methods added: findWord, findNum, endNum, getNum They are needed for easier date search
    Now I will remove the "finish" parameter, because it complicates the code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,118 @@
 #include <iostream>
 #include <curl/curl.h>
 #include <string>
+#include <limits.h>
+
+struct time
+{
+  int day, month, year, hour, min, sec;
+};
+
+int findWord(std::string &str, std::string &word, int start = 0, int finish = -1)
+{
+  if ((start > finish && finish >= 0) || start >= str.size())
+  {
+    std::cout << "findWord() received invalid input data\n";
+    return -1;
+  }
+  if (word == "")
+    return -1;
+  int i = start, j = 0, ret = 0;
+  while (i < str.size() && (i <= finish || finish < 0))
+  {
+    if (str[i] == word[j])
+    {
+      if (j == 0)
+        ret = i;
+      j++;
+      if (j >= word.size())
+        return ret;
+    }
+    else j = 0;
+    i++;
+  }
+  return -1;
+}
+
+int findNum(std::string &str, int start = 0, int finish = -1)
+{
+  if ((start > finish && finish >= 0) || start >= str.size())
+  {
+    std::cout << "findNum() received invalid input data\n";
+    return -1;
+  }
+  int i = start;
+  while (i < str.size() && (i <= finish || finish < 0) && (str[i] < '0' || str[i] > '9'))
+    i++;
+  if (i >= str.size() || (i > finish && finish >= 0))
+  {
+    std::cout << "The string does not contain a number\n";
+    return -1;
+  }
+  return i;
+}
+
+int endNum(std::string &str, int start = 0, int finish = -1)
+{
+  if ((start > finish && finish >= 0) || start >= str.size())
+  {
+    std::cout << "endNum() received invalid input data\n";
+    return -1;
+  }
+  int i = start;
+  while (i < str.size() && (i <= finish || finish < 0) && str[i] >= '0' && str[i] <= '9')
+    i++;
+  if (i >= str.size() || (i > finish && finish >= 0))
+  {
+    std::cout << "the number is at the end of the string\n";
+    return -1;
+  }
+  return i;
+}
+
+int getNum(std::string &str, int start = 0, int finish = -1)
+{
+  if ((start > finish && finish >= 0) || start >= str.size())
+  {
+    std::cout << "getNum() received invalid input data\n";
+    return -1;
+  }
+  int ret = 0;
+  int i = start;
+  while (i < str.size() && (i <= finish || finish < 0) && str[i] >= '0' && str[i] <= '9')
+  {
+    int temp = INT_MAX - (str[i] - '0');
+    if (temp / 10 < ret || (temp / 10 == ret && temp % 10 > 0))
+    {
+      std::cout << "The number in the string exceeds the maximum int value\n";
+      return -1;
+    }
+    else ret = ret * 10 + (str[i] - '0');
+    i++;
+  }
+  return ret;
+}
+
+bool getTime(std::string &str, struct time &_time)
+{
+  const std::string monthName[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  std::string word = "date:";
+  int i = findWord(str, word);
+  if (i == -1)
+  {
+    std::cout << "The string does not contain a date\n";
+    return false;
+  }
+  i = findNum(str, i);
+  _time.day = getNum(str, i);
+  i = endNum(str, i);
+  if (_time.day == -1 || _time.day >= 30)
+  {
+    std::cout << "The string does not contain a date\n";
+    return false;
+  }
+  return true;
+}
 
 static size_t getResponseToString(void* contents, size_t size, size_t nmemb, void* userp)
 {
@@ -29,7 +141,6 @@ bool httpCreator(std::string &strResponse)
     return false;
   }
   return true;
-
 }
 
 int main()
@@ -45,5 +156,6 @@ int main()
         return 0;
     }
     else break;
-  std::cout << strResponse << "\n";
+  struct time _time;
+  getTime(strResponse, _time);
 }
